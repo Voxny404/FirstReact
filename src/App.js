55fc7e4939ceaa07a84ced1638f9037799317bc2
Voxny404 <Atomchen92@gmail.com>
2020-05-29 01:10:52 +0200

    adding speak function and input

--- src/App.js
+++ src/App.js
@@ -2,40 +2,76 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GetTime from './GetTime';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-//import Button from 'react-bootstrap/Button';
+//import DropdownButton from 'react-bootstrap/DropdownButton';
+//import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
 //import { Button } from "./components/Button";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.speak("")
+  }
 
-  return (
-    <div className="App">
-      <div style={{ display: "flex" }}>
-        <DropdownButton id="dropdown-basic-button" title="|||" style={{ marginLeft: "auto" }}>
-          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </DropdownButton>
-      </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <GetTime />
-        <p>
 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+  myChangeHandler = (event) => {
+    this.setState({value: event.target.value});
 
-        </a>
-      </header>
-    </div>
-  );
+  }
+
+speak = (textToSpeak) =>{
+  var msg = new SpeechSynthesisUtterance();
+  var voices = window.speechSynthesis.getVoices();
+  msg.voice = voices[4];
+  msg.text = textToSpeak;
+  msg.lang = "en-US";
+  speechSynthesis.speak(msg);
 
 }
+
+  render(){
+
+
+
+    return (
+      <div className="App">
+
+        <header className="App-header">
+        <div style={{ display: "flex" }}>
+
+        </div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <GetTime />
+          <br />
+          <textarea value={this.state.value}
+            type="text" placeholder="Type something"
+            style={{backgroundColor: 'transparent', border: 'none', outline: 'none', borderBottom: '1px solid black',height:'30px'}}
+            onChange={this.myChangeHandler} onKeyPress={
+              event =>{if(event.key === 'Enter'){this.speak(this.state.value);this.setState({value: ""});}}
+            }/>
+          <br />
+
+          <Button onClick={() => {this.speak(this.state.value);this.setState({value: ""})}} variant="outline-primary">Send</Button>{' '}
+
+          <p>
+
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+
+          </a>
+        </header>
+      </div>
+    );
+
+
+  }
+}
+
 
 export default App;
